5276ca839d2830ff4ee94be040a96c668c111980
fatima1808 <faty_escudero18@hotmail.com>
2019-01-31 16:29:27 -0600

    ultimas modificaciones 31/01/2019

---
+++ js/barra.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var lastScroll;
+
+    $(document).scroll(function(){
+        var nav = $(".navbar");
+        var scrollTop = $(document).scrollTop();
+        if(lastScroll > scrollTop){
+            nav.removeClass("navbar-custom-up");
+            lastScroll = scrollTop;
+        }else if(scrollTop >= $(document).height()/4){
+            nav.addClass("navbar-custom-up");
+            lastScroll = scrollTop;
+        }
+        
+        if(scrollTop >= 120){
+            nav.addClass("navbar-custom-dark");
+        }else {
+            nav.removeClass("navbar-custom-dark");
+        }
+    });
+});
