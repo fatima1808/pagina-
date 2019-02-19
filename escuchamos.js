3e44c7f94dd296f8b793c89d5a0a3be2f44f28b9
fatima <faty_escudero18@hotmail.com>
2019-02-19 11:40:33 -0600

    cambios 19/02/19 splash, home, nosotros, servicios

---
+++ escuchamos.js
@@ -0,0 +1,40 @@
+function debounce(func, wait = 10, immediate = true) {
+    var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+	sliderImages.forEach(sliderImage => {
+		//halfway through image
+		const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+		//bottom of image
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
+		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+		
+			//loop over every image
+		if(isHalfShown && isNotScrolledPast) {
+			sliderImage.classList.add('active');
+		} else {
+			sliderImage.classList.remove('active');
+		}
+	});
+}
+
+
+
+
+
+window.addEventListener('scroll', debounce(checkSlide));
